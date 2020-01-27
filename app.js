78399fde13613de507522d8ab4f18f02656737ad
alrafi <hafisalrafi@gmail.com>
2020-01-28 00:38:28 +0700

    fix id for exp and inc

--- app.js
+++ app.js
@@ -124,12 +124,12 @@ var UIController = (function() {
       if (type === 'exp') {
         element = DOMStrings.expenseContainer;
 
-        html = `<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div>
+        html = `<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div>
         <div class="right clearfix"><div class="item__value">- %value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>`;
       } else if (type === 'inc') {
         element = DOMStrings.incomeContainer;
 
-        html = `<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div>
+        html = `<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div>
         <div class="right clearfix"><div class="item__value">+ %value%</div><div class="item__delete">
         <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></<div></div></div>`;
       }
